c9312e38e19be7d6532e172e8bba006dbaf96c78
Dominika <dominika.kmiecik6@gmail.com>
2021-04-22 21:30:44 +0200

    add dijkstra algorithm on adjacency matrix

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def read_file(file, matrix_list):
     values_list = []
     first_line_input = []
@@ -29,7 +32,7 @@ def create_adjacency_matrix(entered_graph, vertices, edges):
 
 def create_adjacency_list(entered_graph, vertices, edges):
     new_list = {}
-    for vertex in range(0, vertices - 1):
+    for vertex in range(0, vertices):
         for edge in range(0, edges):
             temp_list = []
             if vertex not in new_list:
@@ -41,6 +44,34 @@ def create_adjacency_list(entered_graph, vertices, edges):
     return new_list
 
 
+def dijkstra_on_adjacency_matrix(graph, vertices, start):
+    q = [vertex for vertex in range(0, vertices)]  # graph vertices
+    s = []
+    positive_infinity = math.inf
+    d = [positive_infinity for vertex in range(0, vertices)]
+    d[start] = 0
+    p = [-1 for vertex in range(0, vertices)]
+
+    while len(q) > 0:
+        min_cost_index = 0
+        min_cost = positive_infinity
+        # wartosc ma nalezec do q i byc najmniejsza
+        # nie zmienia sie, gdy nie nalezy do q
+        for edge_cost in range(0, vertices):
+            if d[edge_cost] < min_cost and edge_cost in q:
+                min_cost_index = edge_cost
+                min_cost = d[edge_cost]
+
+        s.append(min_cost_index)
+        q.remove(min_cost_index)
+
+        for vertex in range(0, vertices):
+            if graph[min_cost_index][vertex] != 0 and vertex in q:
+                if d[vertex] > d[min_cost_index] + graph[min_cost_index][vertex]:
+                    d[vertex] = d[min_cost_index] + graph[min_cost_index][vertex]
+                    p[vertex] = min_cost_index
+
+
 if __name__ == '__main__':
     matrix_graph_list = []
 
@@ -50,5 +81,5 @@ if __name__ == '__main__':
     start_vertex = first_line_int[2]
 
     graph_matrix = create_adjacency_matrix(matrix_graph_list, vertex_number, edge_number)
-
     graph_list = create_adjacency_list(matrix_graph_list, vertex_number, edge_number)
+    dijkstra_on_adjacency_matrix(graph_matrix, vertex_number, start_vertex - 1)

b2796e68c360f951d6309701e8c0fbc6465873d6
aerlevsedi <sobasmarta@gmail.com>
2021-05-24 19:31:37 +0200

    For testing

--- Cs/Program.cs
+++ Cs/Program.cs
@@ -7,90 +7,172 @@ namespace Cs
     {
         static void Main(string[] args)
         {
-            Stopwatch sw;
+            int[] sizes = {500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500};
 
-            Console.WriteLine("Graph List");
+        
+        for(int i=0; i<9; i++) {
+            Console.WriteLine("utworz (wpisz jakis znak i enter)");
+            Console.ReadLine();
 
-            GraphList graphList = new GraphList();
-            //graphList.showAdjacencyList();
+            GraphList graphList = new GraphList(sizes[i]);
+            graphList.clearDistAndVisited();
 
-            Console.WriteLine("\nDijkstra:");
-            for (int i = 0; i < 10; i++)
-            {
-                graphList.clearDistAndVisited();
+            Console.WriteLine("lista - dijkstra " + sizes[i] + " (wpisz jakis znak i enter)");
+            Console.ReadLine();
 
-                sw = Stopwatch.StartNew();
-                graphList.dijkstra();
-                sw.Stop();
-                var elapsedTime = sw.Elapsed.TotalMilliseconds;
-                Console.WriteLine("Elapsed time: " + elapsedTime);
-            }
+            graphList.dijkstra();
 
-            //graphList.showDist();
-            Console.WriteLine("\nBellman-Ford:");
-            for (int i = 0; i < 10; i++)
-            {
-                graphList.clearDistAndVisited();
+        }
 
-                sw = Stopwatch.StartNew();
-                graphList.bellmanFord();
-                sw.Stop();
-                var elapsedTime = sw.ElapsedMilliseconds;
-                Console.WriteLine("Elapsed time: " + elapsedTime);
-            }
-            Console.WriteLine("\nFloyd-Warshall:");
-            for (int i = 0; i < 10; i++)
-            {
-                graphList.clearDistAndVisited();
+        for(int i=0; i<9; i++) {
+            Console.WriteLine("utworz (wpisz jakis znak i enter)");
+            Console.ReadLine();
 
-                sw = Stopwatch.StartNew();
-                graphList.floydWarshall();
-                sw.Stop();
-                var elapsedTime = sw.Elapsed.TotalMilliseconds;
-                Console.WriteLine("Elapsed time: " + elapsedTime);
-            }
+            GraphList graphList = new GraphList(sizes[i]);
+            graphList.clearDistAndVisited();
 
-            Console.WriteLine("\nGraph Matrix");
+            Console.WriteLine("lista - bellmanFord " + sizes[i] + " (wpisz jakis znak i enter)");
+            Console.ReadLine();
 
-            GraphMatrix graphMatrix = new GraphMatrix();
-            //graphMatrix.showAdjacencyMatrix();
+            graphList.bellmanFord();
+        }
 
-            Console.WriteLine("\nDijkstra:");
-            for (int i = 0; i < 10; i++)
-            {
-                graphMatrix.clearDistAndVisited();
+        for(int i=0; i<9; i++) {
+            Console.WriteLine("utworz (wpisz jakis znak i enter)");
+            Console.ReadLine();
 
-                sw = Stopwatch.StartNew();
-                graphMatrix.dijkstra();
-                sw.Stop();
-                var elapsedTime = sw.Elapsed.TotalMilliseconds;
-                Console.WriteLine("Elapsed time: " + elapsedTime);
-            }
+            GraphList graphList = new GraphList(sizes[i]);
+            graphList.clearDistAndVisited();
 
-            //graphList.showDist();
-            Console.WriteLine("\nBellman-Ford:");
-            for (int i = 0; i < 10; i++)
-            {
-                graphMatrix.clearDistAndVisited();
+            Console.WriteLine("lista - floydWarshall " + sizes[i] + " (wpisz jakis znak i enter)");
+            Console.ReadLine();
 
-                sw = Stopwatch.StartNew();
-                graphMatrix.bellmanFord();
-                sw.Stop();
-                var elapsedTime = sw.Elapsed.TotalMilliseconds;
-                Console.WriteLine("Elapsed time: " + elapsedTime);
-            }
-            Console.WriteLine("\nFloyd-Warshall:");
-            for (int i = 0; i < 10; i++)
-            {
-                graphMatrix.clearDistAndVisited();
+            graphList.floydWarshall();
+        }
 
-                sw = Stopwatch.StartNew();
-                graphMatrix.floydWarshall();
-                sw.Stop();
-                var elapsedTime = sw.Elapsed.TotalMilliseconds;
-                Console.WriteLine("Elapsed time: " + elapsedTime);
-            }
+        for(int i=0; i<9; i++) {
+            Console.WriteLine("utworz (wpisz jakis znak i enter)");
+            Console.ReadLine();
+
+            GraphMatrix graphMatrix = new GraphMatrix(sizes[i]);
+            graphMatrix.clearDistAndVisited();
+
+            Console.WriteLine("macierz - dijkstra " + sizes[i] + " (wpisz jakis znak i enter)");
+            Console.ReadLine();
+
+            graphMatrix.dijkstra();
+        }
+
+        for(int i=0; i<9; i++) {
+            Console.WriteLine("utworz (wpisz jakis znak i enter)");
+            Console.ReadLine();
+
+            GraphMatrix graphMatrix = new GraphMatrix(sizes[i]);
+            graphMatrix.clearDistAndVisited();
+
+            Console.WriteLine("macierz - bellmanFord " + sizes[i] + " (wpisz jakis znak i enter)");
+            Console.ReadLine();
+
+            graphMatrix.bellmanFord();
+        }
+
+        for(int i=0; i<9; i++) {
+            Console.WriteLine("utworz (wpisz jakis znak i enter)");
+            Console.ReadLine();
+
+            GraphMatrix graphMatrix = new GraphMatrix(sizes[i]);
+            graphMatrix.clearDistAndVisited();
+
+            Console.WriteLine("macierz - floydWarshall " + sizes[i] + " (wpisz jakis znak i enter)");
+            Console.ReadLine();
+
+            graphMatrix.floydWarshall();
+        }
 
         }
     }
 }
+
+// Stopwatch sw;
+
+            // Console.WriteLine("Graph List");
+
+            // GraphList graphList = new GraphList();
+            // //graphList.showAdjacencyList();
+
+            // Console.WriteLine("\nDijkstra:");
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     graphList.clearDistAndVisited();
+
+            //     sw = Stopwatch.StartNew();
+            //     graphList.dijkstra();
+            //     sw.Stop();
+            //     var elapsedTime = sw.Elapsed.TotalMilliseconds;
+            //     Console.WriteLine("Elapsed time: " + elapsedTime);
+            // }
+
+            // //graphList.showDist();
+            // Console.WriteLine("\nBellman-Ford:");
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     graphList.clearDistAndVisited();
+
+            //     sw = Stopwatch.StartNew();
+            //     graphList.bellmanFord();
+            //     sw.Stop();
+            //     var elapsedTime = sw.ElapsedMilliseconds;
+            //     Console.WriteLine("Elapsed time: " + elapsedTime);
+            // }
+            // Console.WriteLine("\nFloyd-Warshall:");
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     graphList.clearDistAndVisited();
+
+            //     sw = Stopwatch.StartNew();
+            //     graphList.floydWarshall();
+            //     sw.Stop();
+            //     var elapsedTime = sw.Elapsed.TotalMilliseconds;
+            //     Console.WriteLine("Elapsed time: " + elapsedTime);
+            // }
+
+            // Console.WriteLine("\nGraph Matrix");
+
+            // GraphMatrix graphMatrix = new GraphMatrix();
+            // //graphMatrix.showAdjacencyMatrix();
+
+            // Console.WriteLine("\nDijkstra:");
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     graphMatrix.clearDistAndVisited();
+
+            //     sw = Stopwatch.StartNew();
+            //     graphMatrix.dijkstra();
+            //     sw.Stop();
+            //     var elapsedTime = sw.Elapsed.TotalMilliseconds;
+            //     Console.WriteLine("Elapsed time: " + elapsedTime);
+            // }
+
+            // //graphList.showDist();
+            // Console.WriteLine("\nBellman-Ford:");
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     graphMatrix.clearDistAndVisited();
+
+            //     sw = Stopwatch.StartNew();
+            //     graphMatrix.bellmanFord();
+            //     sw.Stop();
+            //     var elapsedTime = sw.Elapsed.TotalMilliseconds;
+            //     Console.WriteLine("Elapsed time: " + elapsedTime);
+            // }
+            // Console.WriteLine("\nFloyd-Warshall:");
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     graphMatrix.clearDistAndVisited();
+
+            //     sw = Stopwatch.StartNew();
+            //     graphMatrix.floydWarshall();
+            //     sw.Stop();
+            //     var elapsedTime = sw.Elapsed.TotalMilliseconds;
+            //     Console.WriteLine("Elapsed time: " + elapsedTime);
+            // }
